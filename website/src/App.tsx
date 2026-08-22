21cc8c87ab9cefa87b0c32bfe77d695981fb633a
hidooch980 <83729306+hidooch980@users.noreply.github.com>
2026-08-23 00:13:30 +0330

    Update App.tsx

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,20 +1,15 @@
+import React from "react";
+
+
 function App() {
-return (
-
-
-
-MOLIDO
-
-
-THE LAST SIGNAL
-
-
-SIGNAL DETECTED
-
-
+return React.createElement(
+"main",
+{ className: "molido-app" },
+React.createElement("h1", null, "MOLIDO"),
+React.createElement("h2", null, "THE LAST SIGNAL"),
+React.createElement("p", null, "SIGNAL DETECTED"),
 );
 }
-
 
 
 export default App;
